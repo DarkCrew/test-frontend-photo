664a96848fbc3ddeb8d919ec27e8142e75515036
Kirill Kovalev <kirill.kovalev1502@mail.ru>
2023-03-28 10:40:29 +0300

    init: create react-app and make rules

---
+++ my-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React, { ReactElement } from 'react';
+
+import './App.css';
+import './styles/normalize.css';
+import './styles/index.scss';
+
+const App = (): ReactElement => {
+  return (
+    <div className="App">
+      <p>hh</p>
+    </div>
+  );
+};
+
+export default App;
