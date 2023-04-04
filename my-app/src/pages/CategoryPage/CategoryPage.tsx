4b8d3e5391d58b066fe507e334b9598d08cb4536
Kirill Kovalev <kirill.kovalev1502@mail.ru>
2023-04-04 16:47:44 +0300

    add: add elements and styles to category page

--- my-app/src/pages/CategoryPage/CategoryPage.tsx
+++ my-app/src/pages/CategoryPage/CategoryPage.tsx
@@ -10,12 +10,30 @@ import { PhotoItem } from 'core/api/Models';
 import Header from 'components/Header/Header';
 import Image from 'components/Image/Image';
 
+import orientation_land from '../../assets/images/orientation_land.png';
+import orientation_port from '../../assets/images/orientation_port.png';
+import orientation_standart from '../../assets/images/orientation_standart.png';
+import sizeImage from '../../assets/images/photosize.png';
+
 import styles from './CategoryPage.module.scss';
 
 const API_KEY = '563492ad6f917000010000014640aabb4e9d420cbe1c0df7daf4c2bf';
 
 const CategoryPage = (): ReactElement => {
   const [photos, setPhotos] = React.useState([]);
+  const [orientation, setOrientation] = React.useState(orientation_standart);
+
+  const changeOrientation = (): void => {
+    if (orientation === orientation_standart) {
+      setOrientation(orientation_port);
+    }
+    if (orientation === orientation_port) {
+      setOrientation(orientation_land);
+    }
+    if (orientation === orientation_land) {
+      setOrientation(orientation_standart);
+    }
+  };
 
   React.useEffect(() => {
     if (localStorage.getItem('likes-photo') === null) {
@@ -41,6 +59,20 @@ const CategoryPage = (): ReactElement => {
       <div className={styles.category}>
         <div className="container">
           <div className={styles.container}>
+            <ul className={styles.menu}>
+              <li className={styles.orientation} onClick={changeOrientation}>
+                <img src={orientation} alt="orientation" />
+                Orientation
+              </li>
+              <li>
+                <img src={sizeImage} alt="size" />
+                <select className={styles.size}>
+                  <option>Large</option>
+                  <option>Medium</option>
+                  <option>Small</option>
+                </select>
+              </li>
+            </ul>
             <p className={styles.title} onClick={getPhotos}>
               New Your Photos
             </p>
