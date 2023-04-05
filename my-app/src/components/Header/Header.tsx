b18f5140892053f15e1e87d5a6ee3b7433812ca7
Kirill Kovalev <kirill.kovalev1502@mail.ru>
2023-04-05 19:23:29 +0300

    fix: clear search input when press home button

--- my-app/src/components/Header/Header.tsx
+++ my-app/src/components/Header/Header.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/require-default-props */
 /* eslint-disable @typescript-eslint/naming-convention */
 import React, { ReactElement } from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Logo from 'assets/images/Logo';
+import { changeValue } from 'store/redux/slices/searchSlice';
 
 import Search from 'components/Search/Search';
 
@@ -13,11 +15,17 @@ interface Props {
 }
 
 const Header = ({ search }: Props): ReactElement => {
+  const dispatch = useDispatch();
+
+  const clearSearch = (): void => {
+    dispatch(changeValue(''));
+  };
+
   return (
     <header className={styles.header}>
       <div className="container">
         <nav className={styles.container}>
-          <Link to="/" className={styles.logo}>
+          <Link to="/" className={styles.logo} onClick={clearSearch}>
             <Logo />
           </Link>
           {search && <Search />}
